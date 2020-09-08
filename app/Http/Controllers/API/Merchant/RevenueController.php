dce4c4043940278a7b9ec5a14dfcb8b6d7f59631
dyunas <jonathan.almodiel.quebral@gmail.com>
2020-09-09 01:02:34 +0800

    fixed

--- app/Http/Controllers/API/Merchant/RevenueController.php
+++ app/Http/Controllers/API/Merchant/RevenueController.php
@@ -11,7 +11,7 @@ class RevenueController extends Controller
 	public function getDailyRevenue()
 	{
 		return DB::select('
-            SELECT SUM(subTotal) as totalRevenue
+            SELECT SUM(subTotal - (subTotal * 0.05)) as totalRevenue
             FROM customer_orders
 						WHERE created_at = NOW()
 						AND merchant_id = ' . Auth::user()->merchant->id . '
@@ -22,7 +22,7 @@ class RevenueController extends Controller
 	public function getMonthlyRevenue()
 	{
 		return DB::select('
-            SELECT SUM(subTotal) as totalRevenue
+            SELECT SUM(subTotal - (subTotal * 0.05)) as totalRevenue
             FROM customer_orders
 						WHERE created_at >= DATE_SUB(LAST_DAY(NOW()),INTERVAL DAY(LAST_DAY(NOW())) - 1 DAY) AND created_at <= LAST_DAY(CURDATE())
 						AND merchant_id = ' . Auth::user()->merchant->id . '
