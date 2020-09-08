5df0df46ef390692b88a28654850071c9d3dcee2
dyunas <jonathan.almodiel.quebral@gmail.com>
2020-09-08 20:12:16 +0800

    fixed sql query where result only return 1 dasher

--- app/Http/Controllers/API/Customer/OrderController.php
+++ app/Http/Controllers/API/Customer/OrderController.php
@@ -16,26 +16,26 @@ use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
-  /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function index()
-  {
-    return CustomerOrder::where('customer_id', Auth::user()->customer->id)->orderBy('id', 'desc')->get();
-  }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		return CustomerOrder::where('customer_id', Auth::user()->customer->id)->orderBy('id', 'desc')->get();
+	}
 
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @return \Illuminate\Http\Response
-   */
-  public function store(Request $request)
-  {
-    $rider = DB::select(
-      "SELECT *, (SELECT round(
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		$rider = DB::select(
+			"SELECT *, round(
           (
             (
               acos(
@@ -49,118 +49,116 @@ class OrderController extends Controller
         , 2) as distance
         FROM dasher_statuses
         HAVING distance <= 5
-        LIMIT 1) as distance
-        FROM dasher_statuses
-        WHERE unix_timestamp() - unix_timestamp(updated_at) < 5
-        AND dasher_status = 1
-        ORDER BY distance"
-    );
+				AND unix_timestamp() - unix_timestamp(updated_at) < 5
+				AND dasher_status = 1
+				ORDER BY distance"
+		);
 
-    if (empty($rider)) {
-      return response()->json(['header' => 'Oh no!', 'message' => 'There is no available rider at this moment. Pleast try again later!', 'ok' => false], 201);
-    }
+		if (empty($rider)) {
+			return response()->json(['header' => 'Oh no!', 'message' => 'There is no available rider at this moment. Pleast try again later!', 'ok' => false], 201);
+		}
 
-    DasherStatus::where('dasher_id', $rider[0]->id)->update(['dasher_status' => 0]);
+		DasherStatus::where('dasher_id', $rider[0]->id)->update(['dasher_status' => 0]);
 
-    $order_details = $this->store_order($request, $rider[0]->id);
-    $order_item = $this->store_order_items($request->cart, $order_details->id);
+		$order_details = $this->store_order($request, $rider[0]->id);
+		$order_item = $this->store_order_items($request->cart, $order_details->id);
 
-    $notify = new \stdclass;
+		$notify = new \stdclass;
 
-    $notify->merchant_id = $order_details->merchant_id;
-    $notify->rider_id = $rider[0]->id;
-    $notify->type = 'new order';
-    $notify->message = 'Order Ready! Click here';
-    $notify->path = $order_details->id;
+		$notify->merchant_id = $order_details->merchant_id;
+		$notify->rider_id = $rider[0]->dasher_id;
+		$notify->type = 'new order';
+		$notify->message = 'Order Ready! Click here';
+		$notify->path = $order_details->id;
 
-    event(new PlacedOrder($notify));
+		event(new PlacedOrder($notify));
 
-    return response()->json(['header' => 'Order Placed!', 'message' => 'Your order has been placed!', 'order_id' => $order_details->id, 'ok' => true], 201);
-  }
+		return response()->json(['header' => 'Order Placed!', 'message' => 'Your order has been placed!', 'order_id' => $order_details->id, 'ok' => true], 201);
+	}
 
-  public function store_order($request, $rider_id)
-  {
-    $now = date('Ymd');
-    $uniq = substr(uniqid(mt_rand(), true), 0, 5);
+	public function store_order($request, $rider_id)
+	{
+		$now = date('Ymd');
+		$uniq = substr(uniqid(mt_rand(), true), 0, 5);
 
-    return CustomerOrder::create([
-      'order_id'     => 'CR-' . $now . '-' . $uniq,
-      'customer_id'  => $request->customerID,
-      'merchant_id'  => $request->merchantID,
-      'dasher_id'    => $rider_id,
-      'status'       => 'Order placed',
-      'custLat'      => $request->custLat,
-      'custLong'     => $request->custLong,
-      'merchLat'     => $request->merchLat,
-      'merchLong'    => $request->merchLong,
-      'location'     => $request->location,
-      'instruction'  => $request->instruction,
-      'subTotal'     => $request->subTotal,
-      'distance'     => $request->distance,
-      'delivery_fee' => $request->deliveryFee,
-      'total'        => $request->total,
-      'paymentMode'  => $request->paymentMode,
-      'opened'       => 1
-    ]);
-  }
+		return CustomerOrder::create([
+			'order_id'     => 'CR-' . $now . '-' . $uniq,
+			'customer_id'  => $request->customerID,
+			'merchant_id'  => $request->merchantID,
+			'dasher_id'    => $rider_id,
+			'status'       => 'Order placed',
+			'custLat'      => $request->custLat,
+			'custLong'     => $request->custLong,
+			'merchLat'     => $request->merchLat,
+			'merchLong'    => $request->merchLong,
+			'location'     => $request->location,
+			'instruction'  => $request->instruction,
+			'subTotal'     => $request->subTotal,
+			'distance'     => $request->distance,
+			'delivery_fee' => $request->deliveryFee,
+			'total'        => $request->total,
+			'paymentMode'  => $request->paymentMode,
+			'opened'       => 1
+		]);
+	}
 
-  public function store_order_items($cart, $orderid)
-  {
-    $items = [];
+	public function store_order_items($cart, $orderid)
+	{
+		$items = [];
 
-    foreach ($cart as $item) {
-      $items[] = [
-        'prod_id'  => $item['id'],
-        'order_id' => $orderid,
-        'name'     => $item['name'],
-        'qty'      => $item['qty'],
-        'price'    => $item['price']
-      ];
-    }
+		foreach ($cart as $item) {
+			$items[] = [
+				'prod_id'  => $item['id'],
+				'order_id' => $orderid,
+				'name'     => $item['name'],
+				'qty'      => $item['qty'],
+				'price'    => $item['price']
+			];
+		}
 
-    return OrderItemDetails::insert($items);
-  }
+		return OrderItemDetails::insert($items);
+	}
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function show($id)
-  {
-    return OrderCollection::collection(
-      CustomerOrder::where('customer_id', Auth::user()->customer->id)
-        ->where('id', $id)
-        ->get()
-    )->toJson();
-  }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		return OrderCollection::collection(
+			CustomerOrder::where('customer_id', Auth::user()->customer->id)
+				->where('id', $id)
+				->get()
+		)->toJson();
+	}
 
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function update(Request $request, $id)
-  {
-    CustomerOrder::where('id', $id)
-      ->update([
-        'status' => $request->data['status']
-      ]);
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		CustomerOrder::where('id', $id)
+			->update([
+				'status' => $request->data['status']
+			]);
 
-    return response()->json(['message' => 'Order Status Updated!'], 200);
-  }
+		return response()->json(['message' => 'Order Status Updated!'], 200);
+	}
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function destroy($id)
-  {
-    //
-  }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
 }
